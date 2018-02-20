1ba8420dec6adb78216595bf4e4d0b0827abe038
Brent Abrahams <brent_abrahams@yahoo.com>
2018-02-20 08:35:51 +0800

    Updated dependencies

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'constellation:plugins',
-  version: '0.4.7',
+  version: '0.4.8',
   summary: 'A self-service menu of Constellation plugins',
   git: 'https://github.com/JackAdams/constellation-plugins',
   documentation: 'README.md',
@@ -12,7 +12,7 @@ Package.onUse(function(api) {
   api.use(['check', 'underscore']);
   api.use(['templating', 'reactive-dict'], 'client');
   // api.use('ddp', 'server');
-  api.use('constellation:console@1.4.4');
+  api.use('constellation:console@1.4.6');
   api.addFiles(['constellation-plugins.css', 'constellation-plugins.html', 'constellation-plugins-client.js'], 'client');
   api.addFiles(['constellation-plugins-server.js'], 'server');
   
